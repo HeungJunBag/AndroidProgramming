736ed135eadb8c78987236cd48847e0d41b3fe33
HeungJunBag <p2005hihi@gmail.com>
2024-06-18 23:13:56 +0900

    안드로이드프로그래밍_미니프로젝트_2171499_박흥준_D반

---
+++ app/src/main/java/com/example/chatactivity/MessageModel.java
@@ -0,0 +1,51 @@
+package com.example.chatactivity;
+
+public class MessageModel {
+
+    private String messageId;
+    private String senderId;
+    private String message;
+    private long timestamp;
+
+    public MessageModel(String messageId, String senderId, String message, long timestamp) {
+        this.messageId = messageId;
+        this.senderId = senderId;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public MessageModel() {
+    }
+
+    public String getMessageId() {
+        return messageId;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setMessageId(String messageId) {
+        this.messageId = messageId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
